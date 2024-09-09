be949a146f0b089909d6bfc600547272e649dc0d
Ronnie Llasus <llasus.ronnie@gmail.com>
2024-09-09 15:27:18 +0800

    changed icon

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,15 @@
 import { signIn, signOut } from "next-auth/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "../app/home/page";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function App() {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Home />
     </>
   );
