96c475bad7691f9dbe057d860db2875330205320
Ronnie Llasus <llasus.ronnie@gmail.com>
2024-09-19 18:51:03 +0800

    google rendering in navbar

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import "../../styles/navbar/navbar.css";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
 
 //images
 import logout from "../../../../public/images/navbar/navbar-logout.png";
@@ -11,6 +12,7 @@ import userIcon from "../../../../public/images/navbar/sidenav-user.png";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const { data: session } = useSession();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -40,19 +42,47 @@ export default function Navbar() {
         </div>
 
         <div className="navbar-right">
-          {/* Navbar Subtitle */}
-          <h1 className="navbar-subtitle">Logout</h1>
-          <Image src={logout} alt="" className="logout-icon" />
+          {session ? (
+            <>
+              <button onClick={() => signOut()} className="navbar-subtitle">
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link href="/signinoption">
+                <h1 className="navbar-subtitle">Login</h1>
+              </Link>
+            </>
+          )}
         </div>
       </div>
 
       {/* Sidenav */}
       <div className={`sidenav ${isOpen ? "open" : ""}`}>
         <div className="sidenav-account">
-          <Image src={userIcon} alt="" className="logout-icon" />
-          <Link href="/signinoption" className="view-account">
-            View Account
-          </Link>
+          {session ? (
+            <>
+              <Image
+                src={session.user.image || userIcon}
+                alt="User Icon"
+                className="logout-icon"
+                width={96}
+                height={96}
+              />
+              <p>{session.user.name}</p>
+              <button onClick={() => signOut()} className="logout-button">
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Image src={userIcon} alt="User Icon" className="logout-icon" />
+              <Link href="/signinoption" className="view-account">
+                View Account
+              </Link>
+            </>
+          )}
         </div>
 
         <div className="sidenav-links">
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter, Poppins } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
+import SessionProviderWrapper from "./SessionProviderWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
+      </body>
     </html>
   );
 }
