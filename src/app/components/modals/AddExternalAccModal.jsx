8f803b3d021e7d5dd9a1b345d8ae69ab8f612e0e
Ronnie Llasus <llasus.ronnie@gmail.com>
2024-10-21 21:36:17 +0800

    imported cancel confirmation modal

--- src/app/components/modals/AddExternalAccModal.jsx
+++ src/app/components/modals/AddExternalAccModal.jsx
@@ -10,6 +10,7 @@ export default function AddAccModal(props) {
   const [email, setEmail] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [isEmailValid, setIsEmailValid] = useState(false);
+  const [showCancelConfirmation, setShowCancelConfirmation] = useState(false);
 
   const handleEmailChange = (e) => {
     const emailValue = e.target.value;
@@ -56,95 +57,97 @@ export default function AddAccModal(props) {
   };
 
   const handleCancel = () => {
+    setShowCancelConfirmation(true);
+  };
+
+  const handleConfirmCancel = () => {
     setEmail("");
     setAccessToken("");
     setIsEmailValid(false);
+    setShowCancelConfirmation(false);
     props.onHide();
   };
 
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      className="addacc-modal-overlay"
-    >
-      <Modal.Header className="addacc-modal-header">
-        <Modal.Title
-          id="contained-modal-title-vcenter"
-          className="addacc-modal-title"
-        >
-          Add an Account
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="addacc-modal-body">
-        <Form>
-          <Form.Group
-            className="mb-3 form-group-with-icon"
-            controlId="formEmail"
+    <>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        className="addacc-modal-overlay"
+      >
+        <Modal.Header className="addacc-modal-header">
+          <Modal.Title
+            id="contained-modal-title-vcenter"
+            className="addacc-modal-title"
           >
-            <img
-              src="/images/modals/addacc-email.png"
-              alt="Email Icon"
-              className="form-icon-email"
-            />
-            <Form.Control
-              type="email"
-              placeholder="User Email"
-              value={email}
-              onChange={handleEmailChange}
-              autoFocus
-              className="form-control-with-icon"
-            />
-          </Form.Group>
-
-          <Form.Group
-            className="mb-3 form-group-with-icon access-token-group"
-            controlId="formAccessToken"
-          >
-            <img
-              src="/images/modals/addacc-key.png"
-              alt="Token Icon"
-              className="form-icon-token"
-            />
-            <Form.Control
-              type="text"
-              placeholder="Access Token"
-              value={formatToken(accessToken)}
-              onChange={handleAccessTokenChange}
-              className="form-control-with-icon"
-              readOnly
-            />
-            <Button
-              onClick={handleGenerateToken}
-              className="btn generate-token-btn"
-              disabled={!isEmailValid}
+            Add an Account
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="addacc-modal-body">
+          <Form>
+            <Form.Group
+              className="mb-3 form-group-with-icon"
+              controlId="formEmail"
             >
-              Generate Token
-            </Button>
-          
-            {/* <Button
-              onClick={handleSendEmail}
-              className="btn send-email-btn"
-              disabled={!isEmailValid || !accessToken}
+              <img
+                src="/images/modals/addacc-email.png"
+                alt="Email Icon"
+                className="form-icon-email"
+              />
+              <Form.Control
+                type="email"
+                placeholder="User Email"
+                value={email}
+                onChange={handleEmailChange}
+                autoFocus
+                className="form-control-with-icon"
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3 form-group-with-icon access-token-group"
+              controlId="formAccessToken"
             >
-              Send Email
-            </Button> */}
+              <img
+                src="/images/modals/addacc-key.png"
+                alt="Token Icon"
+                className="form-icon-token"
+              />
+              <Form.Control
+                type="text"
+                placeholder="Access Token"
+                value={formatToken(accessToken)}
+                onChange={handleAccessTokenChange}
+                className="form-control-with-icon"
+                readOnly
+              />
+              <Button
+                onClick={handleGenerateToken}
+                className="btn generate-token-btn"
+                disabled={!isEmailValid}
+              >
+                Generate Token
+              </Button>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer className="addacc-modal-footer">
+          <Button onClick={handleCancel} className="btn cancel">
+            Cancel
+          </Button>
+          <Button onClick={handleAddAccount} className="btn addacc">
+            Add Account
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-          </Form.Group>
-
-
-        </Form>
-      </Modal.Body>
-      <Modal.Footer className="addacc-modal-footer">
-        <Button onClick={handleCancel} className="btn cancel">
-          Cancel
-        </Button>
-        <Button onClick={handleAddAccount} className="btn addacc">
-          Add Account
-        </Button>
-      </Modal.Footer>
-    </Modal>
+      <CancelConfirmationModal
+        show={showCancelConfirmation}
+        onHide={() => setShowCancelConfirmation(false)}
+        onConfirm={handleConfirmCancel}
+      />
+    </>
   );
 }
