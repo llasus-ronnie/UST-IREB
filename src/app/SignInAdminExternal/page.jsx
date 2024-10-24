ab474d77fdda5d2cf80eff493c95ddf889120abe
ceskaflores <franceskaflores*@gmail.com>
2024-10-24 20:47:47 +0800

    tweaked content

--- src/app/SignInAdminExternal/page.jsx
+++ src/app/SignInAdminExternal/page.jsx
@@ -15,106 +15,104 @@ import USTLogo from "../../../public/images/signin/USTLogo.png";
 import "../styles/signin/SignInAdminExternal.css";
 
 export default function SignIn() {
-  const [isRecaptchaVerified, setIsRecaptchaVerified] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { data: session, status } = useSession();
-  const router = useRouter();
+const [isRecaptchaVerified, setIsRecaptchaVerified] = useState(false);
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const { data: session, status } = useSession();
+const router = useRouter();
 
-  useEffect(() => {
+useEffect(() => {
     if (status === "authenticated") {
-      const userRole = session.user.role;
-      if (userRole === "IREB") {
+    const userRole = session.user.role;
+    if (userRole === "IREB") {
         router.push("/IREB/IREBDashboard");
-      } else if (userRole === "PrimaryReviewer") {
+    } else if (userRole === "PrimaryReviewer") {
         router.push("/PrimaryReviewer/PRDashboard");
-      } else if (userRole === "REC") {
+    } else if (userRole === "REC") {
         router.push("/REC/RECdashboard/[rec]");
-      } else {
+    } else {
         router.push("../Unauthorized");
-      }
     }
-  }, [session, status, router]);
+    }
+}, [session, status, router]);
 
-  const handleRecaptchaChange = (value) => {
+const handleRecaptchaChange = (value) => {
     setIsRecaptchaVerified(!!value);
-  };
+};
 
-  const handleSignIn = () => {
+const handleSignIn = () => {
     signIn("credentials", { email, password }); // Use the appropriate sign-in method
-  };
+};
 
-  return (
+return (
     <>
-      <div className="admin-signin-container">
+    <div className="admin-signin-container">
         <Image
-          src={bgAdmin}
-          alt="Background"
-          layout="fill"
-          className="admin-bg"
+        src={bgAdmin}
+        alt="Background"
+        layout="fill"
+        className="admin-bg"
         />
 
         <div className="admin-signin-logo-container">
-          <Image src={USTLogo} alt="UST Logo" className="admin-logo" />
-          <h1 className="admin-title">
+        <Image src={USTLogo} alt="UST Logo" className="admin-logo" />
+        <h1 className="admin-title">
             <b>UST IREB</b>
             <br />
             <span className="research-portal-text">Research Portal</span>
-          </h1>
+        </h1>
         </div>
 
         <div className="admin-signin-box">
-          <h1 className="admin-signin-title">External Reviewer Sign In</h1>
-          <p className="admin-signin-text">
-          Welcome, External Reviewer! To access the features of the UST IREB Research Portal, please enter your credentials below.</p>
+        <h1 className="admin-signin-title">External Reviewer Sign In</h1>
+        <p className="admin-signin-text">
+        Welcome, External Reviewer! To access the features of the UST IREB Research Portal, please enter your credentials below.</p>
 
-          <input
+        <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             className="admin-input"
-          />
+        />
 
-          {/* Password Input */}
-          <input
+        <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             className="admin-input"
-          />
+        />
 
-          <div className="recaptcha-wrapper">
+        <div className="recaptcha-wrapper">
             <ReCAPTCHA
-              className="admin-recaptcha"
-              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-              size="normal"
-              onChange={handleRecaptchaChange}
+            className="admin-recaptcha"
+            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+            size="normal"
+            onChange={handleRecaptchaChange}
             />
-          </div>
+        </div>
 
-          {/* Sign In Button */}
-          <button
+        <button
             onClick={handleSignIn}
             disabled={!isRecaptchaVerified}
             className="admin-signin-button"
-          >
+        >
             Sign In
-          </button>
+        </button>
 
-          <hr />
+        <hr />
 
-          <div className="admin-signin-footer">
+        <div className="admin-signin-footer">
             <p>
-              Need help signing in? <a href="#">Learn More</a>
+            Need help signing in? <a href="#">Learn More</a>
             </p>
             <p>
-              <a href="/">Return to Home</a>
+            <a href="/">Return to Home</a>
             </p>
-          </div>
         </div>
-      </div>
+        </div>
+    </div>
     </>
-  );
+);
 }
