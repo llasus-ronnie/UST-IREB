256f3080c8f96e593f538ea07ccad8d236d4f883
triciamonique <triciamonique.cuaresma@gmail.com>
2024-10-24 22:45:28 +0800

    updated adminnav icons

--- src/app/components/navbaradmin/RecNav.jsx
+++ src/app/components/navbaradmin/RecNav.jsx
@@ -28,59 +28,63 @@ const RecNav = (props) => {
         <ul>
           <li>
             <Link href="../../REC/RECUserProfile" passHref>
-              <div>
-                <Image
-                  src="/images/adminnav/adminnav-account.png"
-                  alt="Account"
-                  width={32}
-                  height={32}
-                />
-              </div>
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#fcbf15" className="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+              </svg>
+            </div>
               {isOpen && <span>My Profile</span>}
             </Link>
           </li>
 
           <li>
             <Link href="../REC/RECdashboard/rec" passHref>
-              <div>
-                <Image
-                  src="/images/adminnav/adminnav-home.png"
-                  alt="Home"
-                  width={32}
-                  height={32}
-                />
-              </div>
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#fcbf15" className="bi bi-house-door" viewBox="0 0 16 16">
+                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
+              </svg>
+            </div>
               {isOpen && <span>Home</span>}
             </Link>
           </li>
 
           <li>
             <Link href={`/REC/RECSubmissions/${props.rec}`} passHref>
-              <div>
-                <Image
-                  src="/images/adminnav/adminnav-submissions.png"
-                  alt="Submissions"
-                  width={32}
-                  height={32}
-                />
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#fcbf15" className="bi bi-clipboard-check" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
+                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+              </svg>
+            </div>
                 {isOpen && <span>Submissions</span>}
-              </div>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/content" passHref>
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#fcbf15" className="bi bi-pencil-square" viewBox="0 0 16 16">
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+            </svg>
+            </div>
+                {isOpen && <span>Content</span>}
             </Link>
           </li>
 
           <li>
             <Link href="/reports" passHref>
-              <div>
-                <Image
-                  src="/images/adminnav/adminnav-reports.png"
-                  alt="Reports"
-                  width={32}
-                  height={32}
-                />
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#fcbf15" className="bi bi-folder2" viewBox="0 0 16 16">
+                <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5z"/>
+            </svg>
+            </div>
                 {isOpen && <span>Reports</span>}
-              </div>
             </Link>
           </li>
+
         </ul>
       </div>
 
@@ -91,15 +95,14 @@ const RecNav = (props) => {
               href="/#"
               onClick={() => signOut({ callbackUrl: "/SignInAdmin" })}
             >
-              <div>
-                <Image
-                  src="/images/adminnav/adminnav-logout.png"
-                  alt="Log Out"
-                  width={32}
-                  height={32}
-                />
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none">
+                <path d="M21 12L13 12" stroke="#fcbf15" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9" stroke="#fcbf15" stroke-width= "2" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19" stroke="#fcbf15" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
+            </div>
                 {isOpen && <span>Log Out</span>}
-              </div>
             </Link>
           </li>
         </ul>
