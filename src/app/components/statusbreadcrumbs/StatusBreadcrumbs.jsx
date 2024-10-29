e8116dfa53adb99453a7956e0a2b16384c7d9584
Moonie <iellemizuki@gmail.com>
2024-10-29 16:00:47 +0800

    Status Breadcrumbs complete

--- src/app/components/statusbreadcrumbs/StatusBreadcrumbs.jsx
+++ src/app/components/statusbreadcrumbs/StatusBreadcrumbs.jsx
@@ -2,12 +2,12 @@
 
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import "../../styles/statusbreadcrumbs/StatusBreadcrumbs.css";
+import "../../styles/statusbreadcrumbs/StatusBreadcrumbs.scss";
 import axios from "axios";
 
 const CheckMarkSVG = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-check-lg" viewBox="0 0 16 16">
-    <path 
+    <path
       d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"
       fill="white"
       stroke="white"
@@ -36,7 +36,7 @@ export default function StatusBreadcrumbs({ steps = [], params }) {
         const response = await axios.get(`/api/forms/${params.id}`);
         console.log("Response data:", response.data);
         setForm(response.data.submission);
-        console.log("Form data:", response.data.submission); 
+        console.log("Form data:", response.data.submission);
       } catch (error) {
         console.error("Error fetching data:", error);
         setError("Failed to fetch form details.");
@@ -51,11 +51,10 @@ export default function StatusBreadcrumbs({ steps = [], params }) {
     }
   }, [params]);
 
-
   // Determine the index of the active step based on form.status
-  const activeStep = steps.findIndex(step => step.id === form?.status); 
+  const activeStep = steps.findIndex(step => step.id === form?.status);
 
-   return (
+  return (
     <>
       {isClient && (
         <>
@@ -63,8 +62,12 @@ export default function StatusBreadcrumbs({ steps = [], params }) {
             {steps.map((step, index) => {
               return (
                 <div key={index} className={`breadcrumbs-step-container ${index < activeStep ? 'completed' : ''}`}>
-                  <div className="breadcrumbs-step-line"></div>
-                  <div className={`breadcrumbs-step-circle ${form && form.status === step.id ? 'active' : ''} ${index < activeStep ? 'completed' : ''}`}></div>
+                  <div className={`breadcrumbs-step-line ${index === activeStep - 1 ? 'yellow' : ''}`}></div>
+                  <div className={`breadcrumbs-step-circle ${form && form.status === step.id ? 'active' : ''} ${index < activeStep ? 'completed' : ''}`}>
+                    {form && form.status === step.id && (
+                      <div className="white-circle"></div>
+                    )}
+                  </div>
                   <div className="breadcrumbs-step-content">
                     <div className={`breadcrumbs-step-title ${form && form.status === step.id ? 'active' : ''} ${index < activeStep ? 'completed' : ''}`}>{step.title}</div>
                     {step.description && (
