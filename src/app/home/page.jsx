27b6046f15ea59ebd250e2f8c956f49c4139f511
ceskaflores <franceskaflores*@gmail.com>
2024-10-31 16:57:59 +0800

    card carousel bootstrap flop!!!!!

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -111,103 +111,36 @@ function Home() {
             <div className="section-4">
               <Container>
                 <Row className="row-alignment">
-                  {/* column 1 */}
-                  <Col>
+
+                  <CardCarousel />
+
+                  <Image
+                      src={flowchart}
+                      alt="flowchart"
+                      className="section-4-flowchart"
+                    />
+                    
+                  {/* <Col>
                     <Image
                       src={whitefaqicon}
                       alt="faq icon"
                       className="section-4-faq"
                     />
-                    <h1>Process of Submission</h1>
-
-                    <p>
-                      This section describes the submission procedures of an
-                      application for research ethics review to the UST
-                      Institutional Research Ethics Board (IREB).
-                    </p>
-
-                    <p>
-                      1. If the principal investigator is within UST, they can
-                      access the portal using their UST Gmail account. If the
-                      principal investigator is outside UST, they must request
-                      an account from IREB by submitting a letter of intent.
-                      Once endorsed, IREB will provide a unique token to access
-                      the portal. Click here to learn more about creating an
-                      account.
-                    </p>
-
-                    <p>
-                      2. The principal investigator reviews the Research Ethics
-                      Committee (REC) initial requirements of the chosen college
-                      where the research paper will be submitted.
-                    </p>
-
-                    <p>
-                      3. When logged in at the UST IREB Portal, the principal
-                      investigator must complete the online forms and upload all
-                      the required documents in PDF format.
-                    </p>
-
-                    <p>
-                      4. The REC evaluates the submitted requirements. If the
-                      requirements meet REC standards, the principal
-                      investigator must process the payment fee for the chosen
-                      REC and submit proof of payment.
-                    </p>
-
-                    <p>
-                      5. The submission will then be classified as expedited,
-                      exempt, or subject to a full board review, depending on
-                      the nature of the research. If the submission undergoes a
-                      full board or expedited review, the REC designates a
-                      primary reviewer to assess the content of the research
-                      paper.
-                    </p>
-
-                    <p>
-                      6. If revisions are requested by the primary reviewer, the
-                      principal investigator must resubmit the necessary
-                      revisions in the portal. If they wish to appeal the
-                      requested revision, they can do so using the designated
-                      comment section in the resubmission.
-                    </p>
-
-                    <p>
-                      7. The principal investigator has 1-3 weeks to make
-                      revisions. If no resubmissions are received after 1 month,
-                      the research submission will be marked as deferred.
-                    </p>
-
-                    <p>
-                      8. Each resubmission will take around 1-2 weeks to be
-                      reviewed. If no further revisions are required, the paper
-                      proceeds to the final review by the respective REC Chair.
-                    </p>
-
-                    <p>
-                      9. Upon approval, the principal investigator receives a
-                      certificate of ethics review. If disapproved, they receive
-                      a letter of disapproval. If the research is classified as
-                      exempt, a certification of exemption from ethical review
-                      is issued.
-                    </p>
-                  </Col>
+                    <CardCarousel />
+                  </Col> */}
 
                   {/* column 2 */}
-                  <Col>
+                  {/* <Col>
                     <Image
                       src={flowchart}
                       alt="flowchart"
                       className="section-4-flowchart"
                     />
-                  </Col>
+                  </Col> */}
                 </Row>
               </Container>
             </div>
           </div>
-
-          <CardCarousel />
-
           <div className="footer">
             <Footer />
           </div>
