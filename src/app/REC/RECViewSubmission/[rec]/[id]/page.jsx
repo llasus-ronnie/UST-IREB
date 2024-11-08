da5ba678e095dab03c2f2cdf8fc5b0776baad307
triciamonique <triciamonique.cuaresma@gmail.com>
2024-11-08 09:46:50 +0800

    view submission minor update

--- src/app/REC/RECViewSubmission/[rec]/[id]/page.jsx
+++ src/app/REC/RECViewSubmission/[rec]/[id]/page.jsx
@@ -86,6 +86,23 @@ function RECViewSubmission({ params }) {
           </div>
 
             <Row className="viewsubmission-container">
+            <a href="../REC/RECSubmissions" className="back-button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+                />
+              </svg>
+              Go Back to Manage Submissions
+            </a>
+
               <Col xs={12} lg={8} className="viewsub-content-container">
               </Col>
               <Col xs={12} lg={4} className="viewsub-details-container">
