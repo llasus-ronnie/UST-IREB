a807d8ff9f808c50006659b049282de21c510295
triciamonique <triciamonique.cuaresma@gmail.com>
2024-11-08 20:29:00 +0800

    hala naayos ko na yung sa recnav mobile tulog lang pala kailangan HAHAHA

--- src/app/components/navbaradmin/RecNavMobile.jsx
+++ src/app/components/navbaradmin/RecNavMobile.jsx
@@ -101,8 +101,8 @@ const RecNavMobile = (props) => {
                 <div>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
+                    width="30"
+                    height="30"
                     fill="#a58324"
                     className={`bi bi-clipboard-check ${
                       isActive === "submissions" ? "active-link" : ""
@@ -125,23 +125,25 @@ const RecNavMobile = (props) => {
               className={`${isActive === "content" ? "active-linkline" : ""}`}
             >
               <a href={`/REC/RECManageContent/${props.rec}`}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  fill="#a58324"
-                  className={`bi bi-pencil-square ${
-                    isActive === "content" ? "active-link" : ""
-                  }`}
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-                  />
-                </svg>
-                <p>Content</p>
+                <div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30"
+                        height="30"
+                        fill="#a58324"
+                        className={`bi bi-pencil-square ${
+                          isActive === "content" ? "active-link" : ""
+                        }`}
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path
+                          fillRule="evenodd"
+                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                        />
+                      </svg>
+                  <p>Content</p>
+                </div>
               </a>
             </li>
 
@@ -152,8 +154,8 @@ const RecNavMobile = (props) => {
                 <div>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
+                    width="30"
+                    height="30"
                     fill="#a58324"
                     className="bi bi-folder2"
                     viewBox="0 0 16 16"
@@ -176,8 +178,8 @@ const RecNavMobile = (props) => {
                 <div>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
+                    width="30"
+                    height="30"
                     viewBox="0 0 24 24"
                     fill="none"
                   >
