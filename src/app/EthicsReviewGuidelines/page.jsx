096f8baa6901dfc0052106fe3d51027d820cbdfa
Ronnie Llasus <llasus.ronnie@gmail.com>
2024-11-22 16:00:11 +0800

    updated ethics review guidelines page

--- src/app/EthicsReviewGuidelines/page.jsx
+++ src/app/EthicsReviewGuidelines/page.jsx
@@ -39,66 +39,6 @@ function EthicsReviewGuidelines() {
 
         <Container className="erg-container">
           <div className="erg-maincontent">
-            <div className="erg-supplementary-forms">
-              <h1>Supplementary Forms</h1>
-
-              <div className="erg-description">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-info-circle"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
-                </svg>
-                <p>
-                  Download the supplementary forms by clicking on the files
-                  below.
-                </p>
-              </div>
-
-              <ul>
-                <li>
-                  <a
-                    href="https://res.cloudinary.com/dyrf8wr1i/raw/upload/v1731218235/Form-2A-Review-Checklist"
-                    download
-                  >
-                    Form-2A-Review-Checklist
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    href="https://res.cloudinary.com/dyrf8wr1i/raw/upload/v1731218683/Form-2B-Registration-and-Application-Form-for-Initial-Review-and-Resubmission"
-                    download
-                  >
-                    Form-2B-Registration-and-Application-Form-for-Initial-Review-and-Resubmission
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    href="https://res.cloudinary.com/dyrf8wr1i/raw/upload/v1731218806/Form-2C-Study-Protocol-Assessment-Form"
-                    download
-                  >
-                    Form-2C-Study-Protocol-Assessment-Form
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    href="https://res.cloudinary.com/dyrf8wr1i/raw/upload/v1731218897/Form-2D-Informed-Consent-Assessment-Form"
-                    download
-                  >
-                    Form-2D-Informed-Consent-Assessment-Form
-                  </a>
-                </li>
-              </ul>
-            </div>
-
             <div className="erg-ethics-guidelines">
               <h1>Ethics Guidelines</h1>
 
