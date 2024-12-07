766e0fce8baeb62a848b25ffe5f641469c3b1e3e
Ronnie Llasus <llasus.ronnie@gmail.com>
2024-12-07 21:50:31 +0800

    added show/hide archive button on manage content

--- src/app/IREB/IREBManageContent/page.jsx
+++ src/app/IREB/IREBManageContent/page.jsx
@@ -26,6 +26,9 @@ function IREBManageContent(props) {
   const [isLoading, setIsLoading] = useState(false);
   const [faqs, setFaqs] = useState([]);
 
+  const [isArchivedShown, setIsArchivedShown] = useState(false);
+  const handleShowArchived = () => setIsArchivedShown((prev) => !prev);
+
   const handleShowAddFAQModal = () => setModalShowAddFAQ(true);
   const handleShowEditFAQModal = (faq) => {
     setSelectedFaq(faq);
@@ -79,10 +82,12 @@ function IREBManageContent(props) {
       setIsLoading(true);
       try {
         const response = await axios.get("/api/IREBContent");
-        console.log("API Response:", response.data);
-        const activeContent = response.data.data.filter(
-          (faq) => !faq.isArchived
-        );
+        const allContent = response.data.data;
+
+        const activeContent = isArchivedShown
+          ? allContent
+          : allContent.filter((faq) => !faq.isArchived);
+
         setContent(activeContent);
         setFilteredContent(activeContent);
       } catch (error) {
@@ -93,7 +98,7 @@ function IREBManageContent(props) {
     }
 
     fetchData();
-  }, [props.modalShowAddFAQ]);
+  }, [isArchivedShown]);
 
   //loading
   const loadingContainerStyle = {
@@ -208,21 +213,23 @@ function IREBManageContent(props) {
                               <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z" />
                             </svg>
                           </button>
-                          <button
-                            className="archive-icon"
-                            onClick={() => handleShowArchiveModal(form)}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="16"
-                              height="16"
-                              fill="currentColor"
-                              className="bi bi-archive"
-                              viewBox="0 0 16 16"
+                          {form.isArchived ? null : (
+                            <button
+                              className="archive-icon"
+                              onClick={() => handleShowArchiveModal(form)}
                             >
-                              <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5zm13-3H1v2h14zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5" />
-                            </svg>
-                          </button>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                className="bi bi-archive"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5zm13-3H1v2h14zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5" />
+                              </svg>
+                            </button>
+                          )}
                         </td>
                       </tr>
                     ))
@@ -235,6 +242,9 @@ function IREBManageContent(props) {
                 </tbody>
               </table>
             </div>
+            <button className="archive-toggle" onClick={handleShowArchived}>
+              {isArchivedShown ? "Hide Archived" : "Show Archived"}
+            </button>
           </div>
         </div>
       </div>
